6c36653e7a10f68976967d5db6d634465a5bc5f4
Kamagalhaes92 <karinemagalhaes92@gmail.com>
2024-03-01 16:58:56 -0800

    Added text-animation for smaller screens

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function imageAppearonScroll() {
+function imageAppearOnScroll() {
   let imageContainer = document.querySelector(".third-section-projects");
   let imageMove = document.querySelector(".image-move");
   let imageMove2 = document.querySelector(".image-move2");
@@ -18,7 +18,7 @@ function imageAppearonScroll() {
   }
 }
 
-function appearOnScroll() {
+function textAppearOnScroll() {
   let textContainer = document.querySelector(".text-animated-container");
   let line1 = document.querySelector(".line1");
   let line2 = document.querySelector(".line2");
@@ -35,8 +35,27 @@ function appearOnScroll() {
   }
 }
 
-window.addEventListener("scroll", imageAppearonScroll);
-window.addEventListener("scroll", appearOnScroll);
+function skillsAppearOnScroll() {
+  let skillsContainer = document.querySelector(".second-section-skills");
+  let h3Elements = skillsContainer.querySelectorAll("h3");
+  let containerPosition = skillsContainer.getBoundingClientRect().top;
+  let windowHeight = window.innerHeight / 1.5;
+
+  if (windowHeight > containerPosition) {
+    h3Elements.forEach((h3) => h3.classList.add("appear"));
+  } else {
+    h3Elements.forEach((h3) => h3.classList.remove("appear"));
+  }
+}
+
+window.addEventListener("scroll", textAppearOnScroll);
+window.addEventListener("resize", textAppearOnScroll);
+
+// Trigger the function on page load
+window.addEventListener("load", textAppearOnScroll);
+
+window.addEventListener("scroll", imageAppearOnScroll);
+window.addEventListener("scroll", skillsAppearOnScroll);
 
 document.addEventListener("DOMContentLoaded", function () {
   // Smooth scrolling for the fourth section
